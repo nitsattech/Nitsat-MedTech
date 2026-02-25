e01a3e405081e1fbce4091fe3d1ae867233fe0d2
nitsattech <nitsattechnologies@gmail.com>
2026-02-25 20:38:28 +0530

    Remove Mongo backend scaffold and standardize project on SQLite only

--- backend/src/modules/auth/auth.controller.js
+++
@@ -1,8 +0,0 @@
-import { asyncHandler } from '../../utils/asyncHandler.js';
-import { login } from './auth.service.js';
-
-export const loginController = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  const data = await login(email, password);
-  res.json(data);
-});
